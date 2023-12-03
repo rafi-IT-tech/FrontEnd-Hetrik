694937037a2fa5a2d75546c19e378d8e5f411b8b
rafi <rafie6195@gmail.com>
2023-12-03 09:55:56 +0700

    hapus populate Room

--- src/pages/dashboard/DashboardPage.jsx
+++ src/pages/dashboard/DashboardPage.jsx
@@ -32,7 +32,11 @@ function DashboardPage() {
     color: 'black',
     textAlign: 'center'
   };
-  // Example API endpoint URLs (replace with your actual URLs)
+
+
+
+  const transaksi = 'https://hetrik-api.onrender.com/api/hitung/usage/';
+
   const productUrl = 'https://hetrik-api.onrender.com/api/device/devices';
   const bangunanUrl = 'https://hetrik-api.onrender.com/api/dayaBangunan/dayabangunan';
   const customerUrl = 'https://api.example.com/customers';
@@ -54,7 +58,7 @@ function DashboardPage() {
       },
   })
       .then(response => 
-        // console.log("Response  ", response.data[0].powerCapacity)
+
         setCategoryCount(response.data[0].powerCapacity)
         )
       .catch(error => console.error('Error fetching category data:', error));
@@ -76,7 +80,15 @@ function DashboardPage() {
       .catch(error => console.error('Error fetching alert data:', error));
 
 
-         // Fetch data for the bar chart
+    axios.get(productUrl,{
+      headers: {
+        'Authorization': `Bearer ${storedToken}`,
+    },
+    })
+    .then(response => setBarChartData(response.data))
+    .catch(error => console.error('Error fetching bar chart data:', error));
+
+
     axios.get(productUrl,{
       headers: {
         'Authorization': `Bearer ${storedToken}`,
@@ -86,50 +98,50 @@ function DashboardPage() {
     .catch(error => console.error('Error fetching bar chart data:', error));
 
   }, []);
-    // const data = [
-    //     {
-    //       name: 'Page A',
-    //       uv: 4000,
-    //       pv: 2400,
-    //       amt: 2400,
-    //     },
-    //     {
-    //       name: 'Page B',
-    //       uv: 3000,
-    //       pv: 1398,
-    //       amt: 2210,
-    //     },
-    //     {
-    //       name: 'Page C',
-    //       uv: 2000,
-    //       pv: 9800,
-    //       amt: 2290,
-    //     },
-    //     {
-    //       name: 'Page D',
-    //       uv: 2780,
-    //       pv: 3908,
-    //       amt: 2000,
-    //     },
-    //     {
-    //       name: 'Page E',
-    //       uv: 1890,
-    //       pv: 4800,
-    //       amt: 2181,
-    //     },
-    //     {
-    //       name: 'Page F',
-    //       uv: 2390,
-    //       pv: 3800,
-    //       amt: 2500,
-    //     },
-    //     {
-    //       name: 'Page G',
-    //       uv: 3490,
-    //       pv: 4300,
-    //       amt: 2100,
-    //     },
-    //   ];
+    const data = [
+        {
+          name: 'Page A',
+          uv: 4000,
+          pv: 2400,
+          amt: 2400,
+        },
+        {
+          name: 'Page B',
+          uv: 3000,
+          pv: 1398,
+          amt: 2210,
+        },
+        {
+          name: 'Page C',
+          uv: 2000,
+          pv: 9800,
+          amt: 2290,
+        },
+        {
+          name: 'Page D',
+          uv: 2780,
+          pv: 3908,
+          amt: 2000,
+        },
+        {
+          name: 'Page E',
+          uv: 1890,
+          pv: 4800,
+          amt: 2181,
+        },
+        {
+          name: 'Page F',
+          uv: 2390,
+          pv: 3800,
+          amt: 2500,
+        },
+        {
+          name: 'Page G',
+          uv: 3490,
+          pv: 4300,
+          amt: 2100,
+        },
+      ];
      
 
   return (
@@ -198,7 +210,7 @@ function DashboardPage() {
                 <LineChart
                 width={500}
                 height={300}
-                data={barChartData}
+                data={data}
                 margin={{
                     top: 5,
                     right: 30,
@@ -211,8 +223,11 @@ function DashboardPage() {
                 <YAxis />
                 <Tooltip />
                 <Legend />
-                <Line type="monotone" dataKey="product_power" stroke="#8884d8" activeDot={{ r: 8 }} />
-                <Line type="monotone" dataKey="product_power" stroke="#82ca9d" />
+                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+
+                {/* <Line type="monotone" dataKey="product_power" stroke="#8884d8" activeDot={{ r: 8 }} />
+                <Line type="monotone" dataKey="product_power" stroke="#82ca9d" /> */}
                 </LineChart>
             </ResponsiveContainer>
 
