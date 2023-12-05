05a2933b32f069fae964b26ad2cfdb3d637f4587
rafi <rafie6195@gmail.com>
2023-12-05 21:03:08 +0700

    Kabeh

--- src/App.jsx
+++ src/App.jsx
@@ -37,12 +37,15 @@ const App = () => {
 
         <Routes>
         <Route path="/" element={<Homepage />}/>
-        <Route path="/About" element={<PrivateRoute element={<About />} />} />
         <Route path="/Calculate" element={<PrivateRoute element={<CalculatorPage />} />} />
         <Route path="/Paymentmain" element={<PrivateRoute element={<Paymentmain />} />} />
 
         <Route path="/Service" element={<PrivateRoute element={<Service />} />} />
-        <Route path="/Article" element={<PrivateRoute element={<Article />} />} />
+        {/* <Route path="/About" element={<PrivateRoute element={<About />} />} /> */}
+        {/* <Route path="/Article" element={<PrivateRoute element={<Article />} />} /> */}
+        <Route path="/Article" element={<Article />} />
+        <Route path="/About" element={<About />} />
+
         <Route path="/Login" element={<Login />} />
         <Route path="/SignUp" element={<Signup />} />
         <Route path="/Dashboard" element={<PrivateRoute element={<DashboardPage />} />} />
